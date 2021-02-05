500e67d4e3c276f442a75b351bc3d3e4d61fc27b
AlexChen0 <75153539+AlexChen0@users.noreply.github.com>
2021-02-05 12:11:47 -0600

    Add files via upload

---
+++ TerminaDora/Assets/DoraMouse.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoraMouse : MonoBehaviour
+{
+    float velocity = 5f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        rb.velocity = new Vector2(0, 0);
+        float iy = Input.GetAxis("Vertical");
+        float ix = Input.GetAxis("Horizontal");
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-velocity, rb.velocity.y);    
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(velocity, rb.velocity.y);    
+        }
+        if(Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, velocity);    
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -velocity);    
+        }
+
+    }
+
+}

fb723ac3a6a3228a6e86e0b340be8d259b7e8e95
Nyle Arora <nylearora2021@u.northwestern.edu>
2021-02-09 00:00:10 -0600

    witch enemy AI

---
+++ TerminaDora/Assets/WitchFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitchFollow : MonoBehaviour
+{
+    public float speed = 1;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}

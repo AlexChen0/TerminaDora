091587d0cb57baf9f529de26cb4edcf8facf0b43
Nyle Arora <nylearora2021@u.northwestern.edu>
2021-02-08 22:43:04 -0600

    cloud enemy added

---
+++ TerminaDora/Assets/cloud.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloud : MonoBehaviour
+{
+	
+    float og_x; 
+    Rigidbody2D rb;
+    public float speed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.right * speed;
+        og_x = rb.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Debug.Log(og_x);
+        if (rb.position.x >= og_x + 2.5){
+        	speed = -2;
+        }
+        if (rb.position.x <= og_x - 2.5){
+        	speed = 2;
+        }
+        rb.velocity = Vector2.right * speed;
+    }
+}

347a4916ee196f0d1204c295882c5e12e21dddc6
Nyle Arora <nylearora2021@u.northwestern.edu>
2021-02-18 21:07:10 -0600

    hearts fixed in place

---
+++ TerminaDora/Assets/HeartSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartSystem : MonoBehaviour
+{
+    public GameObject[] hearts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float xOffset = 0.0f;
+        for (int i = 0; i < hearts.Length; i++){
+        	Vector3 temp = hearts[i].transform.position;
+        	temp.y = transform.position.y + 4.31f; 
+        	temp.x = transform.position.x + 5.5f + xOffset; 
+        	hearts[i].transform.position = temp;
+        	xOffset = xOffset + 0.7f;
+        }
+    }
+}
